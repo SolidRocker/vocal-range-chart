51bb89b0ba1f217fc6f42ce676e6ee864fd27fe4
SolidRocker <kamtk.tyler@gmail.com>
2019-03-08 17:44:54 +0800

    Updates

---
+++ precache-manifest.34c3fc797cf36470075d81235697539a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1247d23ad8483617415f9fb3c9f03cff",
+    "url": "/SolidRocker/vocal-range-chart/static/media/longImage.1247d23a.png"
+  },
+  {
+    "revision": "3937683dc7b12c908e88",
+    "url": "/SolidRocker/vocal-range-chart/static/js/runtime~main.3937683d.js"
+  },
+  {
+    "revision": "6f67b33675f8927d65b4",
+    "url": "/SolidRocker/vocal-range-chart/static/js/main.6f67b336.chunk.js"
+  },
+  {
+    "revision": "3f858582bfec24bcd6d4",
+    "url": "/SolidRocker/vocal-range-chart/static/js/2.3f858582.chunk.js"
+  },
+  {
+    "revision": "6f67b33675f8927d65b4",
+    "url": "/SolidRocker/vocal-range-chart/static/css/main.2b396caa.chunk.css"
+  },
+  {
+    "revision": "493719e7734d824efafcd59bd83bc134",
+    "url": "/SolidRocker/vocal-range-chart/index.html"
+  }
+];
