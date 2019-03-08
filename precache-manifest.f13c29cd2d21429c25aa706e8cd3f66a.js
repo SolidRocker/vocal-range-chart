08b3fa7149a9040dba134b6f1888b89d11031f58
SolidRocker <kamtk.tyler@gmail.com>
2019-03-08 18:05:44 +0800

    Updates

---
+++ precache-manifest.f13c29cd2d21429c25aa706e8cd3f66a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1247d23ad8483617415f9fb3c9f03cff",
+    "url": "/vocal-range-chart/static/media/longImage.1247d23a.png"
+  },
+  {
+    "revision": "ffabf211b1edda9cfeb6",
+    "url": "/vocal-range-chart/static/js/runtime~main.ffabf211.js"
+  },
+  {
+    "revision": "e61b251d620ba1ea80fe",
+    "url": "/vocal-range-chart/static/js/main.e61b251d.chunk.js"
+  },
+  {
+    "revision": "f30b6da194cfa7f9fa66",
+    "url": "/vocal-range-chart/static/js/2.f30b6da1.chunk.js"
+  },
+  {
+    "revision": "e61b251d620ba1ea80fe",
+    "url": "/vocal-range-chart/static/css/main.2b396caa.chunk.css"
+  },
+  {
+    "revision": "ef6e532c8bea9f0e700e7c5cd3737557",
+    "url": "/vocal-range-chart/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/SolidRocker/vocal-range-chart/precache-manifest.34c3fc797cf36470075d81235697539a.js"
+  "/vocal-range-chart/precache-manifest.f13c29cd2d21429c25aa706e8cd3f66a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/SolidRocker/vocal-range-chart/index.html", {
+workbox.routing.registerNavigationRoute("/vocal-range-chart/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
